51775ae7d976020e7f60eb7b3573e20903553104
flowaiwzm <wxhwzm@gmail.com>
2022-03-02 12:36:23 +0800

    master

---
+++ First/src/数组学习/常见操作.java
@@ -0,0 +1,12 @@
+package 数组学习;
+
+public class 常见操作 {
+    public static void main(String[] args) {
+        // 遍历
+        int[] arr={11,22,33,44,55};
+        for(int x=0;x<arr.length;x++){
+            System.out.println(arr[x]);
+        }
+    }
+    
+}
---
+++ First/src/数组学习/常见问题.java
@@ -0,0 +1,9 @@
+package 数组学习;
+
+public class 常见问题 {
+    public static void main(String[] args) {
+        // 索引越界：不存在索引对应的元素
+        // 空指针异常 arr=null
+    }
+    
+}
